a84669da8067cbbd11c769ef1d0f37f53fb3f610
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2019-12-08 21:51:36 -0500

    Add 404 page content

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 import Layout from '../components/Layout';
@@ -6,8 +7,12 @@ import SEO from '../components/SEO';
 export default function Error404Page({ ...restProps }) {
   return (
     <>
-      <SEO title="Error 404: Resource Not found" />
-      <Layout {...restProps}></Layout>
+      <SEO title="404: Not found" />
+      <Layout {...restProps}>
+        <h1>404: Not found</h1>
+        <p>We’re sorry! We couldn’t find the page you are looking for.</p>
+        <Link to="/">Go home</Link>
+      </Layout>
     </>
   );
 }
