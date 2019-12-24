602e8050e70a2c0352ba80bf84ee01456877733b
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2019-12-24 00:35:48 -0500

    Implement dynamic events listing on events page

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,9 +1,25 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
 export default function EventsPage({ ...restProps }) {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulEvent {
+        edges {
+          node {
+            dateAndTime
+            id
+            slug
+            title
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <>
       <SEO title="Events" />
@@ -13,6 +29,13 @@ export default function EventsPage({ ...restProps }) {
           We are more than just Sunday morning worship. Come and join us for one
           of our upcoming social gatherings or special services.
         </p>
+        <ul>
+          {data.allContentfulEvent.edges.map(({ node }) => (
+            <li key={node.id}>
+              {node.title}, {node.dateAndTime}
+            </li>
+          ))}
+        </ul>
       </Layout>
     </>
   );
